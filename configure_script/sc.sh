2fb85fc53927dcef97c4ed99b93ac8fee1198d4a
ankit835 <38327231+ankit835@users.noreply.github.com>
2024-04-20 15:33:34 +0530

    Update sc.sh

--- configure_script/sc.sh
+++ configure_script/sc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Do not do this.
 databricksUrl=https://adb-1851930191878659.19.azuredatabricks.net
-$ADB_HOME/databricks configure --token << EOF
+/var/lib/jenkins/.local/bin/databricks configure --token << EOF
 $databricksUrl
 $DATABRICKS_TOKEN_MAIN
 EOF
