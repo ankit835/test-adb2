7bb831ef0cb712611b8792c66ce63bd62f45738c
ankit835 <38327231+ankit835@users.noreply.github.com>
2024-04-20 15:08:20 +0530

    Create sc.sh

---
+++ configure_script/sc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Do not do this.
+databricksUrl=https://adb-1851930191878659.19.azuredatabricks.net
+databricks configure --token << EOF
+$databricksUrl
+$DATABRICKS_TOKEN_MAIN
+EOF
+
+databricks workspace list
+
+
+
+
+
